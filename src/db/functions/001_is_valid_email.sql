2f0a26dd8e771d3981a50fbd508ec662b3c3da3e
hafiz <hafiz@deriv.com>
2024-12-22 03:29:10 +0800

    change email constraint to validation before encryption

---
+++ src/db/functions/001_is_valid_email.sql
@@ -0,0 +1,7 @@
+-- Create a function to validate email before encryption
+CREATE OR REPLACE FUNCTION is_valid_email(email TEXT)
+RETURNS BOOLEAN AS $$
+BEGIN
+    RETURN email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$';
+END;
+$$ LANGUAGE plpgsql;
